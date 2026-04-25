3b1c5a1886bff214c4b2ea79a6e40f2090113046
jatingupta080105-pixel <jatingupta080105@gmail.com>
2026-04-26 02:16:26 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Largest in Array/largest-in-array.py
@@ -0,0 +1,9 @@
+class Solution:
+    def largest(self, arr):
+        maxi = arr[0]
+        
+        for num in arr:
+            if num > maxi:
+                maxi = num
+                
+        return maxi
